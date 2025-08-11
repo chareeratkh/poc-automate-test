d0a4e80eb9ce9c9e94a60241beed47e7c098f166
chareeratkh <chareerat.kh@paneet.co.th>
2025-08-12 02:42:59 +0700

    edit data

--- tests/api.js
+++ tests/api.js
@@ -2,16 +2,15 @@ export async function getUserResponse(request, userId) {
     return request.get(`/api/users/${userId}`);
   }
   
-  export async function createUserResponse(request, body) {
+export async function createUserResponse(request, body) {
     return request.post('/api/users', { data: body });
   }
   
-
 export async function loginUser(request, body) {
     return request.post('/api/login', { data: body });
   }
   
-  export async function registerUser(request, body) {
+export async function registerUser(request, body) {
     return request.post('/api/register', { data: body });
   }
   
